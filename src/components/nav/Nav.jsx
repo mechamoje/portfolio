4cb3aab08326b3d083c9f4c0ca44d026c5bd83d4
Jéssica <jessy.ard11@gmail.com>
2022-12-16 17:27:53 -0300

    correct mobile nav

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -11,11 +11,9 @@ import {BsMenuButtonWideFill} from 'react-icons/bs'
 export const Nav = () => {
 
   const [activeNav, setActiveNav] = useState('')
-  const [openMobile, setOpenMob] = useState('')
 
   return (
     <nav>
-      <div> <BsMenuButtonWideFill id='mobileMenu' onClick={() => setOpenMob('open') } className={openMobile === 'open' ? 'openmenu' : ''} /></div>
       <a id='home' href="#"  onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''} > <VscHome className='homeicon' /> </a>
       <a id='user' href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''} > <FiUser className='abouticon' /> </a>
       <a id='job' href="#experience" onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}> <CgToolbox className='jobicon' /> </a>
