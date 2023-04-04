3847df28ae99867b37bcd97997cb92dbb1ce84bf
jéssica <jessy.ard11@gmail.com>
2023-04-04 18:34:39 -0300

    fix: updating infos

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,65 +1,77 @@
 import React from "react";
 import "./experience.css";
 import { BsPatchCheckFill } from "react-icons/bs";
-import NextLogo from "../../assets/nextlogo.png"
-import ReactLogo from "../../assets/react.png"
-import jslogo from "../../assets/js.png"
-import htmllogo from "../../assets/html.png"
-import tslogo from "../../assets/ts.png"
-import csslogo from "../../assets/css.svg"
+import NextLogo from "../../assets/nextlogo.png";
+import ReactLogo from "../../assets/react.png";
+import jslogo from "../../assets/js.png";
+import htmllogo from "../../assets/html.png";
+import tslogo from "../../assets/ts.png";
+import csslogo from "../../assets/css.svg";
+import phplogo from "../../assets/phplogo.png";
+import wplogo from "../../assets/wp.png";
 
 function stars(number) {
-  switch (number){
-    case 1:   
-    return '✪'
-    break
-    case 2:   
-    return '✪✪'
-    break
-    case 3:   
-    return '✪✪✪'
-    break
-    case 4:   
-    return '✪✪✪✪'
-    break
-    case 5:   
-    return '✪✪✪✪✪'
-    break
+  switch (number) {
+    case 1:
+      return "✪";
+      break;
+    case 2:
+      return "✪✪";
+      break;
+    case 3:
+      return "✪✪✪";
+      break;
+    case 4:
+      return "✪✪✪✪";
+      break;
+    case 5:
+      return "✪✪✪✪✪";
+      break;
   }
 }
 
-const experience = [ 
+const experience = [
   {
     icon: jslogo,
-    language: 'Javascript',
+    language: "Javascript",
     level: stars(4),
   },
   {
     icon: ReactLogo,
-    language: 'React JS',
+    language: "React JS",
     level: stars(4),
   },
   {
     icon: csslogo,
-    language: 'CSS3',
-    level:stars(4),
+    language: "CSS3",
+    level: stars(4),
   },
   {
     icon: tslogo,
-    language: 'Typescript',
-    level: stars(2),
+    language: "Typescript",
+    level: stars(4),
   },
   {
     icon: NextLogo,
-    language: 'Next js',
-    level:stars(3),
+    language: "Next js",
+    level: stars(4),
   },
   {
     icon: htmllogo,
-    language: 'HTML5',
+    language: "HTML5",
     level: stars(5),
-  }
-]
+  },
+  {
+    icon: phplogo,
+    language: "PHP",
+    level: stars(3),
+  },
+  {
+    icon: wplogo,
+    language: "WordPress",
+    level: stars(3),
+  },
+];
 
 export const Experience = () => {
   return (
@@ -67,19 +79,20 @@ export const Experience = () => {
       <article>
         <h5>Conhecimentos</h5>
         <h2>Minhas habilidades</h2>
-      
       </article>
 
       <div className="container experience__container">
-      <ul id="experience-list"> 
-        {experience.map(({ id, language, level, icon }) => { return ( 
-                <li className="experience-li">
-                <img src={icon} className='li-icon' /> 
+        <ul id="experience-list">
+          {experience.map(({ id, language, level, icon }) => {
+            return (
+              <li className="experience-li">
+                <img src={icon} className="li-icon" />
                 <h4 className="icon-title"> {language} </h4>
                 <small className="text-light"> {level} </small>
-                </li>);
-      })}     
-      </ul>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </section>
   );
